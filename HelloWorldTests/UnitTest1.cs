6f715277ab016bb63e43c5f9c83aab2a3f4f1e9d
Ratan Ghosh <ratan.ghosh@capgemini.com>
2019-05-10 09:09:31 +0530

    Changed Test cases

--- HelloWorldTests/UnitTest1.cs
+++ HelloWorldTests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace HelloWorldTests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("Hello World", Program.CreateMessage());
+            Assert.AreEqual("Hello World from dev testing", Program.CreateMessage());
         }
     }
 }
